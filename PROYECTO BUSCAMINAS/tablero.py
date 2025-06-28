359f90251ba2e656725406d36185269f80d6abbc
danielaurdaneta <urdaneta.daniela@correo.unimet.edu.ve>
2025-06-28 16:13:09 -0400

    Delete PROYECTO BUSCAMINAS directory

--- PROYECTO BUSCAMINAS/tablero.py
+++
@@ -1,9 +0,0 @@
-class tablero:
-    def __init__  (self, filas, columnas, tablero):
-        self.filas = filas 
-        self.columnas = columnas 
-        self.tablero = tablero 
-    def show (self):
-        print (f"{self.filas}{self.columnas}, {self.tablero}")
-    def rellenar_tablero (self):
-        pass
